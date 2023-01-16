ee62ce6e4277a08dbf882ae1c40397c60afe3f1d
roody <roody@roodys-Mac-mini.local>
2023-01-16 13:29:31 +0100

    added flutter_inappwebview blob handler

--- src/zipcelx.js
+++ src/zipcelx.js
@@ -1,40 +1,52 @@
-import JSZip from 'jszip';
-import FileSaver from 'file-saver';
+import JSZip from 'jszip'
+import FileSaver from 'file-saver'
 
-import validator from './validator';
-import generatorRows from './formatters/rows/generatorRows';
+import validator from './validator'
+import generatorRows from './formatters/rows/generatorRows'
 
-import workbookXML from './statics/workbook.xml';
-import workbookXMLRels from './statics/workbook.xml.rels';
-import rels from './statics/rels';
-import contentTypes from './statics/[Content_Types].xml';
-import templateSheet from './templates/worksheet.xml';
+import workbookXML from './statics/workbook.xml'
+import workbookXMLRels from './statics/workbook.xml.rels'
+import rels from './statics/rels'
+import contentTypes from './statics/[Content_Types].xml'
+import templateSheet from './templates/worksheet.xml'
 
 export const generateXMLWorksheet = (rows) => {
-  const XMLRows = generatorRows(rows);
-  return templateSheet.replace('{placeholder}', XMLRows);
-};
+	const XMLRows = generatorRows(rows)
+	return templateSheet.replace('{placeholder}', XMLRows)
+}
 
 export default (config) => {
-  if (!validator(config)) {
-    throw new Error('Validation failed.');
-  }
+	if (!validator(config)) {
+		throw new Error('Validation failed.')
+	}
 
-  const zip = new JSZip();
-  const xl = zip.folder('xl');
-  xl.file('workbook.xml', workbookXML);
-  xl.file('_rels/workbook.xml.rels', workbookXMLRels);
-  zip.file('_rels/.rels', rels);
-  zip.file('[Content_Types].xml', contentTypes);
+	const zip = new JSZip()
+	const xl = zip.folder('xl')
+	xl.file('workbook.xml', workbookXML)
+	xl.file('_rels/workbook.xml.rels', workbookXMLRels)
+	zip.file('_rels/.rels', rels)
+	zip.file('[Content_Types].xml', contentTypes)
 
-  const worksheet = generateXMLWorksheet(config.sheet.data);
-  xl.file('worksheets/sheet1.xml', worksheet);
+	const worksheet = generateXMLWorksheet(config.sheet.data)
+	xl.file('worksheets/sheet1.xml', worksheet)
 
-  return zip.generateAsync({
-    type: 'blob',
-    mimeType:
-      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-  }).then((blob) => {
-    FileSaver.saveAs(blob, `${config.filename}.xlsx`);
-  });
-};
+	return zip
+		.generateAsync({
+			type: 'blob',
+			mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+		})
+		.then((blob) => {
+			if (window.flutter_inappwebview) {
+				var reader = new FileReader()
+				reader.readAsDataURL(blob)
+				reader.onloadend = function () {
+					var dataUrl = reader.result
+					var base64 = dataUrl.split(',')[1]
+
+					window.flutter_inappwebview.callHandler('blobToBase64Handler', base64.toString(), 'png', 'qrCode')
+				}
+			} else {
+				FileSaver.saveAs(blob, `${config.filename}.xlsx`)
+			}
+		})
+}
